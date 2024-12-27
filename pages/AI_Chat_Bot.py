faa6301ecf910a4f9332353e59e0af1425398c32
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2024-12-27 23:05:45 +0000

    Imported fetch_data to Chat Bot Page

--- pages/AI_Chat_Bot.py
+++ pages/AI_Chat_Bot.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 from utils.auth import get_authenticator
+from utils.database import fetch_data
 
 st.set_page_config(
     page_title="AI: Chatting...",
