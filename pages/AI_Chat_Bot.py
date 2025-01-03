7fe5d1308d475a5bc7734535acae3fb1587f721e
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2025-01-03 22:06:12 +0000

    Fixed model name stored in session state

--- pages/AI_Chat_Bot.py
+++ pages/AI_Chat_Bot.py
@@ -187,7 +187,7 @@ else:
     openai.api_key = st.secrets["OPENAI_API_KEY"]
 
     if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
+        st.session_state["openai_model"] = Settings.llm.model
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
